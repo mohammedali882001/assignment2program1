65e931407e495e8b1660bf2338a69c7051e92c25
mohammedali882001 <93165125+mohammedali882001@users.noreply.github.com>
2021-10-30 16:27:38 -0700

    Add files via upload

---
+++ student.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author messi
+ */
+public class student
+{
+    String name;
+    private int studentID;
+    int studentGPA;
+    
+}
